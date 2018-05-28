5f1fdf6106c43235e35a4224b9ee2c2e9116232b
Titas Kundu <titaskundu@Titass-MacBook-Air.local>
2018-05-28 11:39:25 +0530

    testing POST/users/login

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ var {User} = require('./models/user');
 var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -97,7 +97,7 @@ app.patch('/todos/:id', (req, res) => {
   })
 });
 
-//post Users
+// POST /users
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
@@ -111,7 +111,7 @@ app.post('/users', (req, res) => {
   })
 });
 
-app.get('/users/me', authenticate, (req, res) =>{
+app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
